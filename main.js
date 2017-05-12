86d2f94cbf741017d702d42da56219700cdf3fab
Jammy Yang <jammyyang@gmail.com>
2017-05-12 08:40:05 -0700

    cards, weather and trails display mvp done

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var trailChoices;
 var lat;
 var lon;
 var radius = 25;
+var trailTemps = [];
 
 function getLocationTrailWeather() {
     loadMessage();
@@ -30,17 +31,19 @@ function showPosition(position){
 function getTrailData(lat, lon, radius){
     var options = {
         url:'server/trailApi.php?lat='+lat+"&lon="+lon+"&radius="+radius,
-        success: handleSuccess,
+        success: handleTrailSuccess,
         error: handleError,
         dataType: 'json',
         method: 'get',
         crossDomain: true
     };
 
-    function handleSuccess(result){
+    function handleTrailSuccess(result){
         console.log('success', result);
-        trailChoices = result;
-        getWeatherData();
+        trailChoices = result.places;
+        displayCards();
+
+
     }
 
     function handleError(){
@@ -50,36 +53,83 @@ function getTrailData(lat, lon, radius){
     $.ajax(options);
 }
 
-function displayTrails () {
-    for (var i=0; i<trailChoices.object.places.length; i++) {
-        var trailName = object.places[0].name;
+function displayCards () {
+    for (var i=0; i<trailChoices.length; i++) {
+        var trailName = trailChoices[i].name;
+        var cityTemps = trailTemps[i];
+        console.log('city temps,', cityTemps);
+        var cardColumn = $('<div>', {
+            class: "col-sm-3"
+        });
+
+        var cardDiv = $('<div>', {
+            class: "pic"
+        });
+
+        var imgDiv = $('<img>', {
+            src: "https://c1.staticflickr.com/7/6204/6047319257_b27c1be597_m.jpg"
+        });
+
+        var picBottomDiv = $('<div>', {
+            class: "picBottom"
+        });
+
+
+        var trailNameDiv = $('<div>', {
+            class: "trailName d-inline",
+            text: trailName
+        });
+
+
+        var trailTempDiv = $('<div>', {
+            class: "trailTemp d-inline",
+            text: cityTemps
+        });
+
+        picBottomDiv.append(trailNameDiv, trailTempDiv);
+        cardDiv.append(imgDiv, picBottomDiv);
+        cardColumn.append(cardDiv);
+        $('.row').append(cardColumn);
+        getWeatherData(trailChoices[i],trailTempDiv);
     }
 
+
+}
+function convertDegreesKToF(kTemp){
+    return kTemp * 9/5 - 459.67;
 }
 
-function getWeatherData (){
+function getWeatherData (trail,displayElement){
+
+
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/forecast?id=5257570&APPID=0b8c405de2b767fa2d70b9d5a5325856",
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + trail.city + ",us&APPID=0b8c405de2b767fa2d70b9d5a5325856",
         method: "GET",
         dataType: 'json',
         // async: true,
         // crossDomain: true,
         success: function(result){
-            console.log("This is the weather result", result);
-            var weather = result;
-            for(var i=0; i<weather.city.length; i++){
-
-                var weather_city = weather.city.name;
-                // var temp = weather.list[i].main.temp;
-                var weatherDiv = $('<div>').text(weather_city);
-                $('#hello').append(weather_city);
-                var weather_city = weather.city.country;
-                displayWeather(weather_city, )
-
-            }
+            console.log("This is the weather result", result.main.temp);
+            trailTemps.push(result.main.temp);
+            var degreesSymbol = $("<sup>",{
+                html:'&#8457;',
+                class: 'degreesSymbol'
+            });
+            displayElement.text(Math.floor(convertDegreesKToF(result.main.temp)));
+            displayElement.append(degreesSymbol)
+            var weatherIcon = $("<img>",{
+                src: "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png",
+                class: 'weatherIcon'
+            });
+            displayElement.append(weatherIcon);
+            console.log(trail);
+            //displayElement.append();
         }
-    });
+    })
+
+    console.log(trailTemps);
 }
+
 
 
 // function displayWeather() {
@@ -87,40 +137,40 @@ function getWeatherData (){
 //
 // }
 
-function displayWeather() {
+// function displayWeather() {
+//
+//
+// }
+//
+//
+// function getFlickerData(){
+//     var options = {
+//         url:'',
+//         success: handleSuccess,
+//         error: handleError,
+//         dataType: 'json',
+//         method: 'get'
+//     };
+//
+//     function handleSuccess(result){
+//         console.log('success', result);
+//         pictureChoices = result;
+//     }
+//
+//     function handleError(){
+//         console.log('error');
+//     }
 
+    // $.ajax(options);
+// }
 
-}
-
-
-function getFlickerData(){
-    var options = {
-        url:'',
-        success: handleSuccess,
-        error: handleError,
-        dataType: 'json',
-        method: 'get'
-    };
-
-    function handleSuccess(result){
-        console.log('success', result);
-        pictureChoices = result;
-    }
-
-    function handleError(){
-        console.log('error');
-    }
-
-    $.ajax(options);
-}
-
-function displayTrails () {
-    for (var i=0; i<trailChoices.object.places.length; i++) {
-        var trailName = trailChoices.object.places[i].name;
-        var trailDescription = trailChoices.object.places[i].description;
-        var trailCity = trailChoices.object.places[i].location;
-    }
-}
+// function displayTrails () {
+//     for (var i=0; i<trailChoices.object.places.length; i++) {
+//         var trailName = trailChoices.object.places[i].name;
+//         var trailDescription = trailChoices.object.places[i].description;
+//         var trailCity = trailChoices.object.places[i].location;
+//     }
+// }
 
 
 
--- server/trailApi.php
+++ server/trailApi.php
@@ -13,7 +13,7 @@ if(empty($_GET['radius'])){
 }
 
 require_once('unirest-php/src/Unirest.php');
-$response = Unirest\Request::get("https://trailapi-trailapi.p.mashape.com/?lat={$_GET['lat']}&limit=25&lon={$_GET['lon']}&radius={$_GET['radius']}",
+$response = Unirest\Request::get("https://trailapi-trailapi.p.mashape.com/?lat={$_GET['lat']}&limit=12&lon={$_GET['lon']}&radius={$_GET['radius']}",
     array(
         "X-Mashape-Key" => "ZuznoNfktdmshBlpfoXELOmwwRHsp1ljBH7jsn6zEzOUYtqdYp",
         "Accept" => "text/plain"
